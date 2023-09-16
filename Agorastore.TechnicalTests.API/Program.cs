de29af41f6fe6eb4c36968b924008cf83d81dbac
soufianboutaib <soufian.boutaib@fwa.eu>
2023-09-16 18:02:33 +0200

    Config Program.cs with Swagger and authorizing
    Adding EndPointApi
    Add Swager to be used later in the documentation
    Add Controllers
    AddCors to autorise access from front

--- Agorastore.TechnicalTests.API/Program.cs
+++ Agorastore.TechnicalTests.API/Program.cs
@@ -1,6 +1,21 @@
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseCors(builder =>
+{
+    builder.AllowAnyOrigin()
+           .AllowAnyMethod()
+           .AllowAnyHeader();
+});
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
 
 app.Run();
