9fccadc85796cd54672badc0b05b966609b49baa
Lucas Pussacq <lucas.pussacq@gmail.com>
2023-09-17 17:12:59 +0200

    First pricer implementation

---
+++ Agorastore.TechnicalTests.Tests/PricerTests.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace Agorastore.TechnicalTests.Domain.Tests
+{
+    public class PricerTests
+    {
+        [Fact]
+        public void Price_should_be_compute_according_to_pricing_formula()
+        {
+            var pricer = new Pricer();
+
+            var sellingPrice = pricer.CalculateSellingPrice(10.00m);
+
+            sellingPrice.Should().Be(10.00m + (10.00m * 0.2m));
+        }
+    }
+}
---
+++ Agorastore.TechnicalTests/Pricer.cs
@@ -0,0 +1,10 @@
+﻿namespace Agorastore.TechnicalTests
+{
+    public class Pricer
+    {
+        public decimal CalculateSellingPrice(decimal initialPrice)
+        {
+            return initialPrice + (initialPrice * 0.2m);
+        }
+    }
+}
